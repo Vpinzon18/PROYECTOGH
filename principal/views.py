658be12d115532cda7c9dde8d7d5c55cd3c69832
Alvaro Chamorro G <87681339+alchagiro@users.noreply.github.com>
2023-09-07 17:38:47 -0500

    conexion base de datos

--- principal/views.py
+++ principal/views.py
@@ -11,3 +11,15 @@ def inicio(request):
 def usuarios(request):
     context={}
     return render(request, 'usuarios.html', context)
+
+def formulario(request):
+    context={}
+    return render(request, 'formulario.html', context) 
+
+def datos(request):
+    context={}
+    return render(request, 'datos.html', context)
+
+def certificados(request):
+    context={}
+    return render(request, 'certificados.html', context)
